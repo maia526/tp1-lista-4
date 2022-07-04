6b128b7b9ec30e42436c0e3441660842fa360050
Mariana Maia <96018595+maia526@users.noreply.github.com>
2022-07-04 16:42:53 -0300

    Create ex_23.cpp

---
+++ ex_23.cpp
@@ -0,0 +1,43 @@
+// Leia duas matrizes de inteiros a e b (3x3). Em seguida, gere e imprima a matriz c = a x b.
+#include <stdio.h>
+int main()
+{
+    int a[3][3], b[3][3], c[3][3];
+    //ler a
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("a[%d][%d]: ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+    
+    //ler b    
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("b[%d][%d]: ", i, j);
+            scanf("%d", &b[i][j]);
+        }
+    }
+    
+    //gerar c = a + b
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            c[i][j] = a[i][j] * b[i][j];
+        }
+    }
+    
+    //imprimir c
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("\nc[%d][%d]: %d", i, j, c[i][j]);
+        }
+    }
+}
